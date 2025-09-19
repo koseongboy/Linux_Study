e0e8f2c4a92ecf1cd421b41b83f538a54c124f4d
koseongboy <koseonghyeon08@gmail.com>
2025-09-19 10:33:26 +0900

    test completed

--- practice3.sh
+++ practice3.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ $1 -gt 0 && $2 -gt 0 ]
-	then
-		echo "got it!!"
-	else
-		echo "Input must be greater than 0"
-
+if [ $1 -gt 0 -a $2 -gt  0 ]
+then
+	echo "got it!! the param is $1 and $2"
+else
+	echo "Input must be greater than 0 the param is $1 amd $2"
+fi
 exit 0
